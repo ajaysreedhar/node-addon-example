a8f091c223a9094c77ce3d388f65abeee3518658
Ajay Sreedhar <sree.ajay468@outlook.com>
2019-08-31 02:18:47 +0530

    change: nodejslogo to nodejs-logo png

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const SDLAddon = require('./build/Release/sdladdon');
+const image = __dirname + '/nodejs-logo.png';
+
+/* Every 2 seconds we print to show that event loop is not blocked. */
+setInterval(function () {
+    console.log('Event loop tick...');
+}, 2000);
+
+SDLAddon.createWindow({ image: image, width: 800, height: 600 });
