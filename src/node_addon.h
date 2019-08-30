d36fce923a191884cfde14a72f9282fbd101ea1c
Ajay Sreedhar <sree.ajay468@outlook.com>
2019-08-31 01:21:55 +0530

    add: declare addon functions in namespace

--- src/node_addon.h
+++ src/node_addon.h
@@ -1,8 +1,53 @@
-//
-// Created by ajay on 31/08/19.
-//
+/**
+ * Copyright (c) 2019 Ajay Sreedhar
+ *
+ * Distributed under MIT License. Please refer the LICENCE file.
+ */
+#ifndef EXAMPLE_NODE_ADDON_H
+#define EXAMPLE_NODE_ADDON_H
 
-#ifndef NODEJS_SDL_EXAMPLE_NODE_ADDON_H
-#define NODEJS_SDL_EXAMPLE_NODE_ADDON_H
+#include <iostream>
+#include <string>
 
-#endif //NODEJS_SDL_EXAMPLE_NODE_ADDON_H
+#include <node.h> /* Node.js API implementations */
+#include <v8.h>   /* V8 Javascript engine helpers. */
+
+namespace nsdl {
+
+/**
+ * A traditional C structure to hold window specifications.
+ *
+ * Declares window width, window height and
+ * the path to the image to be displayed.
+ */
+typedef struct {
+    int width = 600, height = 800;
+    std::string* image {};
+} spec_t;
+
+/**
+ * Inflates the entire process. Schedules events on
+ * event loop and dedicated thread.
+ *
+ * @param attr window attributes
+ * @return true or false
+ */
+bool inflate (spec_t attr);
+
+/**
+ * Implements the exported createWindow method.
+ *
+ * @param args V8 callback info
+ */
+void Method (const v8::FunctionCallbackInfo<v8::Value> &args);
+
+/**
+ * Exports nsdl::Method() as "createWindow". This can be used with require() module.
+ *
+ * @param exports injected exports param
+ */
+void Initialize (v8::Local<v8::Object> exports);
+
+} /* namespace nsdl */
+
+#endif /* EXAMPLE_NODE_ADDON_H */
